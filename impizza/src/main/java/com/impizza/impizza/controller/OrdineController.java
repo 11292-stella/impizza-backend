d20a3c17761dd2d1009edaece9a6907f12afd5de
11292-stella <stella.marucelli@gmail.com>
2025-10-09 08:19:47 +0200

    Messaggio del commit

--- impizza/src/main/java/com/impizza/impizza/controller/OrdineController.java
+++ impizza/src/main/java/com/impizza/impizza/controller/OrdineController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ordini") // ✅ rotta corretta
+@RequestMapping("/api/ordini")
 public class OrdineController {
 
     @Autowired
